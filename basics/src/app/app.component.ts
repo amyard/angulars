13e5e549f8c3d8a894138c146eee837b13d89ff2
amyard <maksymturenko@gmail.com>
2023-01-16 02:12:04 +0200

    basics. pipes and directives.

--- basics/src/app/app.component.ts
+++ basics/src/app/app.component.ts
@@ -8,8 +8,22 @@ import { Component } from "@angular/core";
   //styles: ['']
 })
 export class AppComponent {
-  name = 'Maksym'
+  name = 'maksym turenko'
   imgUrl = 'https://i.picsum.photos/id/237/500/500.jpg?hmac=idOEkrJhLd7nEU5pNrAGCyJ6HHJdR_sit1qDt5J3Wo0'
+  images = [
+    'https://i.picsum.photos/id/237/500/500.jpg?hmac=idOEkrJhLd7nEU5pNrAGCyJ6HHJdR_sit1qDt5J3Wo0',
+    'https://i.picsum.photos/id/237/500/500.jpg?hmac=idOEkrJhLd7nEU5pNrAGCyJ6HHJdR_sit1qDt5J3Wo0',
+    'https://i.picsum.photos/id/237/500/500.jpg?hmac=idOEkrJhLd7nEU5pNrAGCyJ6HHJdR_sit1qDt5J3Wo0'
+  ]
+  currentDate = new Date()
+  cost = 2000
+  temperature = 25.3
+  pizza = {
+    toppings: ['pepperoni', 'bacon'],
+    size: 'large'
+  }
+  blueClass = false
+  fontSize = 16
 
   getName() {
     return this.name
