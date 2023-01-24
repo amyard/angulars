9b4a470d16180acdf8d5d6fa4c3ebbac6bc16045
amyard <maksymturenko@gmail.com>
2023-01-24 20:38:30 +0200

    pipeable operators

--- rjxs_intro/src/index.js
+++ rjxs_intro/src/index.js
@@ -1,4 +1,4 @@
-import { Observable, interval, timer, fromEvent } from 'rxjs';
+//import { Observable, interval, timer, fromEvent } from 'rxjs';
 
 // Observable basic concept
 // const observable = new Observable((subsriber) => {
@@ -66,7 +66,7 @@ setTimeout(() => {
 // )
 
 
-import { of } from 'rxjs';
+// import { of } from 'rxjs';
 
 // sync operator
 // const observable = of(1,2,3,4,5)
@@ -85,12 +85,110 @@ import { of } from 'rxjs';
 
 
 
-import { from } from 'rxjs';
+// import { from } from 'rxjs';
 // will flatten all data 
 // const observable = from([1,2,3,4,5])
 
 // also worked with response data
-const observable = from(fetch('https://jsonplaceholder.typicode.com/todos/1'))
+// const observable = from(fetch('https://jsonplaceholder.typicode.com/todos/1'))
+
+// const subscription = observable.subscribe({
+//     next(value) {
+//         console.log(value)
+//     },
+//     complete() {
+//         console.log('complete')
+//     }
+// })
+
+
+
+/* PIPEABLE  OPERATORS */
+// import { of } from 'rxjs';
+// import { map } from 'rxjs/operators';
+
+// const observable = of(1,2,3,4,5)
+
+// const numbersWithSymbols = observable.pipe(
+//     map((value) => `$${value}`)
+// )
+
+
+// const subscription = numbersWithSymbols.subscribe({
+//     next(value) {
+//         console.log(value)
+//     },
+//     complete() {
+//         console.log('complete')
+//     }
+// })
+
+
+import { fromEvent, of, interval } from 'rxjs';
+import { map, filter, reduce, take, scan, tap } from 'rxjs/operators';
+
+// const observable = fromEvent(
+//     document, 'keydown'
+// ).pipe(
+//     map(event => event.code)
+// )
+
+
+// const observable = fromEvent(
+//     document, 'keydown'
+// ).pipe(
+//     map(event => {
+//         return event.code === 'Space' ? event.code : null
+//     })
+
+//     // map(event => event.code),
+//     // filter(code => code === 'Space') // will display only SPACE
+// )
+
+// return 15
+// const observable = of(1,2,3,4,5).pipe(
+//     reduce(
+//         (acc, val) => acc + val,
+//         0
+//     )
+// )
+
+// return 10
+// const observable = interval(500).pipe(
+//     take(5),
+//     reduce(
+//         (acc, val) => acc + val,
+//         0
+//     )
+// )
+
+
+// return 0,1,3,6,10
+// const observable = interval(500).pipe(
+//     take(5),
+//     scan(
+//         (acc, val) => acc + val,
+//         0
+//     )
+// )
+
+
+// return 0,1,2,3,4,10
+const observable = interval(500).pipe(
+    take(5),
+    // both show the same result
+    //tap(console.log),
+    tap({
+        next(val) {
+            console.log(val)
+        }
+    }),
+    reduce(
+        (acc, val) => acc + val,
+        0
+    )
+)
+
 
 const subscription = observable.subscribe({
     next(value) {
